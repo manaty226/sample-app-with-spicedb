86b7b4a0439ddba66dd5c2afe42961c2bd5f57dc
manaty226 <ta.manaty0226@gmail.com>
2022-09-06 20:30:53 +0900

    add: add http server runner

--- main.go
+++ main.go
@@ -1,4 +1,25 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/manaty226/sample-app-with-spicedb/internal/handler"
+	"github.com/manaty226/sample-app-with-spicedb/internal/service"
+	"github.com/manaty226/sample-app-with-spicedb/store"
+)
+
+func runner() *gin.Engine {
+	r := gin.Default()
+	blogs := service.BlogRepository(store.Blogs)
+	a := handler.AddBlog{Service: &service.AddBlogService{Store: &blogs}}
+	g := handler.GetBlog{Service: &service.GetBlogService{Store: &blogs}}
+
+	r.POST("/blogs", a.Handle)
+	r.GET("/blogs/:id", g.Handle)
+
+	return r
+}
+
 func main() {
+	r := runner()
+	r.Run(":3000")
 }
